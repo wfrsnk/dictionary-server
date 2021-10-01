dcc8490de66937f2c2dd84e4d1c4b33c7dcaa67b
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-10-02 00:37:25 +0300

    update CSS

--- dictionary-server-app/src/components/Header.jsx
+++ dictionary-server-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="container">
+    <div className="container head">
       <h2>DictionaryServer</h2>
       <h3>ВКР: "СРАВНИТЕЛЬНЫЙ АНАЛИЗ ПОДХОДОВ К РЕАЛИЗАЦИИ АДАПТИВНОСТИ В ВЕБ-ИНТЕРФЕЙСАХ"</h3>
     </div>
